b781fc4c0853bf2e3a5fcb0432ddc61cda2138e6
Jeú <54154628+jfs-dev@users.noreply.github.com>
2022-09-14 15:32:04 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run(typeof(BenchmarkDoAndWhile).Assembly);
+
+public class BenchmarkDoAndWhile
+{
+    [Benchmark]
+    public void UsingDo()
+    {
+        int n = 0;
+        do
+        {
+            n++;
+        } while (n < 200000);
+    }
+
+    [Benchmark]
+    public void UsingWhile()
+    {
+        int n = 0;
+        while (n < 200000)
+        {
+            n++;
+        }
+    }
+}
